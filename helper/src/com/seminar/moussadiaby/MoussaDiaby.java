1a76e9704456652bdf8165cffcb4b203b61f89e0
mansamusa14 <mansamusadiaby14@gmail.com>
2021-04-06 13:28:20 +0200

    Initial Commit

---
+++ helper/src/com/seminar/moussadiaby/MoussaDiaby.java
@@ -0,0 +1,17 @@
+package com.seminar.moussadiaby;
+
+public class MoussaDiaby {
+}
+
+class Maths {
+    public int add(int first, int second) {
+        int sum = first + second;
+        return sum;
+    }
+
+    public int sub(int first, int second) {
+        int minus = second - first;
+        return minus;
+    }
+
+}
---
+++ student/src/com/seminar/moussadiaby/MoussaDiaby.java
@@ -0,0 +1,19 @@
+package com.seminar.moussadiaby;
+
+public class MoussaDiaby {
+
+}
+
+class Maths {
+    public int add(int first, int second) {
+        int sum = first + second;
+        return sum;
+    }
+
+    public int sub(int first, int second) {
+        int minus = second - first;
+        return minus;
+    }
+
+}
+
